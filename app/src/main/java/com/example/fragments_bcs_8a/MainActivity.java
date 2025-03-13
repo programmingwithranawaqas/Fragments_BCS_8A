006dd526cece3834175bccbe7ccef27c2f2f2a42
Rana Waqas <programmingwithranawaqas@gmail.com>
2025-03-13 14:56:08 +0500

    Populate listview with adapter on fragments

--- app/src/main/java/com/example/fragments_bcs_8a/MainActivity.java
+++ app/src/main/java/com/example/fragments_bcs_8a/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.fragments_bcs_8a;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +12,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.FragmentManager;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements ListFrag.OnContactNameClicked {
+
+    ArrayList<Contact> contacts;
 
     LinearLayout portrait, landscape;
     FragmentManager manager;
+
+    // hooks of detail fragment views
+    TextView tvPhone, tvName, tvAddress;
+    View viewOfDetailFragment;
 
     ListFrag listFrag;
     DetailFrag detailFrag;
@@ -48,8 +58,37 @@ public class MainActivity extends AppCompatActivity {
     {
         landscape = findViewById(R.id.landscape);
         portrait = findViewById(R.id.portrait);
+
+
         manager = getSupportFragmentManager();
         listFrag = (ListFrag) manager.findFragmentById(R.id.listfrag);
         detailFrag = (DetailFrag) manager.findFragmentById(R.id.detailfrag);
+        viewOfDetailFragment = detailFrag.getView();
+
+        tvPhone = viewOfDetailFragment.findViewById(R.id.tvPhoneNumber);
+        tvName = viewOfDetailFragment.findViewById(R.id.tvName);
+        tvAddress = viewOfDetailFragment.findViewById(R.id.tvAddress);
+
+        contacts = new ArrayList<>();
+        contacts.add(new Contact("Waqas","333","444"));
+        contacts.add(new Contact("Ali Raza","222","333"));
+        contacts.add(new Contact("Muzammil hussain","111","222"));
+        contacts.add(new Contact("Kashif Zafar","000","111"));
+        contacts.add(new Contact("Kashif Zafar","000","111"));
+        contacts.add(new Contact("Kashif Zafar","000","111"));
+        contacts.add(new Contact("Kashif Zafar","000","111"));
+        contacts.add(new Contact("Kashif Zafar","000","111"));
+        contacts.add(new Contact("Kashif Zafar","000","111"));
+
+        onContactNameItemClicked(0);
+    }
+
+    @Override
+    public void onContactNameItemClicked(int position) {
+        Contact contact = contacts.get(position);
+        tvAddress.setText(contact.getAddress());
+        tvPhone.setText(contact.getPhone());
+        tvName.setText(contact.getName());
+
     }
 }
